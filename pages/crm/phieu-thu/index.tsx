e693c8ad9406fbf811ceb92206fab8e843bb0339
LongTien15 <nguyentienlong.150800@gmail.com>
2023-08-17 08:32:43 +0700

    fix build err

--- pages/crm/phieu-thu/index.tsx
+++ pages/crm/phieu-thu/index.tsx
@@ -1,48 +1,43 @@
-import { SidebarContext } from "@/components/crm/context/resizeContext";
-import styleHome from "@/components/crm/home/home.module.css";
-import { SetStateAction, useContext, useEffect, useRef, useState } from "react";
-import { useHeader } from "@/components/crm/hooks/useHeader";
+import { SidebarContext } from '@/components/crm/context/resizeContext'
+import styleHome from '@/components/crm/home/home.module.css'
+import { SetStateAction, useContext, useEffect, useRef, useState } from 'react'
+import { useHeader } from '@/components/crm/hooks/useHeader'
 
-import { TableRowSelection } from "antd/es/table/interface";
-import HomeList from "./@/components/crm/delete_data/delete_data.list";
-import HomeFollow from "@/components/crm/theo-doi-thu-chi/theo-doi-thu-chi";
-import HomePhieuThu from "@/components/crm/theo-doi-thu-chi/phieu-thu";
-
+import HomePhieuThu from '@/components/crm/theo-doi-thu-chi/phieu-thu'
 
 export default function TheoDoiThuChi() {
-  const mainRef = useRef<HTMLDivElement>(null);
-  const { isOpen } = useContext<any>(SidebarContext);
-  const [selected, setSelected] = useState(false);
-  const [numberSelected, setNumberSelected] = useState(0);
-  const [selectedRowKeys, setSelectedRowKeys] = useState<any>([]);
+  const mainRef = useRef<HTMLDivElement>(null)
+  const { isOpen } = useContext<any>(SidebarContext)
+  const [selected, setSelected] = useState(false)
+  const [numberSelected, setNumberSelected] = useState(0)
+  const [selectedRowKeys, setSelectedRowKeys] = useState<any>([])
   const {
     headerTitle,
     setHeaderTitle,
     setShowBackButton,
     setCurrentPath,
-  }: any = useHeader();
+  }: any = useHeader()
   const handleDeselectAll = () => {
-    setSelectedRowKeys([]);
-    setNumberSelected(0);
-  };
+    setSelectedRowKeys([])
+    setNumberSelected(0)
+  }
   useEffect(() => {
-    setHeaderTitle("Phiếu thu");
-    setShowBackButton(false);
-    setCurrentPath("/crm/phieu-thu");
-
-  }, [setHeaderTitle, setShowBackButton, setCurrentPath]);
+    setHeaderTitle('Phiếu thu')
+    setShowBackButton(false)
+    setCurrentPath('/crm/phieu-thu')
+  }, [setHeaderTitle, setShowBackButton, setCurrentPath])
 
   useEffect(() => {
     if (isOpen) {
-      mainRef.current?.classList.add("content_resize");
+      mainRef.current?.classList.add('content_resize')
     } else {
-      mainRef.current?.classList.remove("content_resize");
+      mainRef.current?.classList.remove('content_resize')
     }
-  }, [isOpen]);
-  
+  }, [isOpen])
+
   return (
     <div ref={mainRef} className={styleHome.main}>
       <HomePhieuThu />
     </div>
-  );
+  )
 }
--- pages/crm/theo-doi-thu-chi/index.tsx
+++ pages/crm/theo-doi-thu-chi/index.tsx
@@ -1,47 +1,43 @@
-import { SidebarContext } from "@/components/crm/context/resizeContext";
-import styleHome from "@/components/crm/home/home.module.css";
-import { SetStateAction, useContext, useEffect, useRef, useState } from "react";
-import { useHeader } from "@/components/crm/hooks/useHeader";
+import { SidebarContext } from '@/components/crm/context/resizeContext'
+import styleHome from '@/components/crm/home/home.module.css'
+import { SetStateAction, useContext, useEffect, useRef, useState } from 'react'
+import { useHeader } from '@/components/crm/hooks/useHeader'
 
-import { TableRowSelection } from "antd/es/table/interface";
-import HomeList from "./@/components/crm/delete_data/delete_data.list";
-import HomeFollow from "@/components/crm/theo-doi-thu-chi/theo-doi-thu-chi";
-
+import HomeFollow from '@/components/crm/theo-doi-thu-chi/theo-doi-thu-chi'
 
 export default function TheoDoiThuChi() {
-  const mainRef = useRef<HTMLDivElement>(null);
-  const { isOpen } = useContext<any>(SidebarContext);
-  const [selected, setSelected] = useState(false);
-  const [numberSelected, setNumberSelected] = useState(0);
-  const [selectedRowKeys, setSelectedRowKeys] = useState<any>([]);
+  const mainRef = useRef<HTMLDivElement>(null)
+  const { isOpen } = useContext<any>(SidebarContext)
+  const [selected, setSelected] = useState(false)
+  const [numberSelected, setNumberSelected] = useState(0)
+  const [selectedRowKeys, setSelectedRowKeys] = useState<any>([])
   const {
     headerTitle,
     setHeaderTitle,
     setShowBackButton,
     setCurrentPath,
-  }: any = useHeader();
+  }: any = useHeader()
   const handleDeselectAll = () => {
-    setSelectedRowKeys([]);
-    setNumberSelected(0);
-  };
+    setSelectedRowKeys([])
+    setNumberSelected(0)
+  }
   useEffect(() => {
-    setHeaderTitle("Theo dõi chi thu");
-    setShowBackButton(false);
-    setCurrentPath("/crmtheo-doi-thu-chi");
-
-  }, [setHeaderTitle, setShowBackButton, setCurrentPath]);
+    setHeaderTitle('Theo dõi chi thu')
+    setShowBackButton(false)
+    setCurrentPath('/crmtheo-doi-thu-chi')
+  }, [setHeaderTitle, setShowBackButton, setCurrentPath])
 
   useEffect(() => {
     if (isOpen) {
-      mainRef.current?.classList.add("content_resize");
+      mainRef.current?.classList.add('content_resize')
     } else {
-      mainRef.current?.classList.remove("content_resize");
+      mainRef.current?.classList.remove('content_resize')
     }
-  }, [isOpen]);
-  
+  }, [isOpen])
+
   return (
     <div ref={mainRef} className={styleHome.main}>
       <HomeFollow />
     </div>
-  );
+  )
 }
