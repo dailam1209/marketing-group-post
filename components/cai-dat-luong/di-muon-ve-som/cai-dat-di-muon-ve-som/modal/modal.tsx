4444884815f3c64760f4a482a7bdaa3c069f8b85
LongTien15 <nguyentienlong.150800@gmail.com>
2023-08-18 20:17:10 +0700

    fix ui

--- components/cai-dat-luong/di-muon-ve-som/cai-dat-di-muon-ve-som/modal/modal.tsx
+++ components/cai-dat-luong/di-muon-ve-som/cai-dat-di-muon-ve-som/modal/modal.tsx
@@ -1,9 +1,9 @@
-import { Modal, Form, Select, Input, Button } from "antd"
-import Image from "next/image"
-import styles from "./modal.module.css"
-import { POST_TL } from "@/pages/api/BaseApi"
-import moment from "moment"
-import { useRouter } from "next/router"
+import { Modal, Form, Select, Input, Button } from 'antd'
+import Image from 'next/image'
+import styles from './modal.module.css'
+import { POST_TL } from '@/pages/api/BaseApi'
+import moment from 'moment'
+import { useRouter } from 'next/router'
 
 export const ModalXoaCaiDaiDMVS = (
   open: boolean,
@@ -14,8 +14,8 @@ export const ModalXoaCaiDaiDMVS = (
 ) => {
   const router = useRouter()
   const onDel = async () => {
-    const res = await POST_TL("api/tinhluong/congty/delete_phat_muon", {
-      pm_id: selectedRow?.["pm_id"]
+    const res = await POST_TL('api/tinhluong/congty/delete_phat_muon', {
+      pm_id: selectedRow?.['pm_id'],
     })
 
     if (res?.data) {
@@ -32,51 +32,47 @@ export const ModalXoaCaiDaiDMVS = (
       onCancel={() => setOpen(false)}
       width={500}
       closable={false}
-      cancelButtonProps={{ style: { display: "none" } }}
-      okButtonProps={{ style: { display: "none" } }}
-    >
+      cancelButtonProps={{ style: { display: 'none' } }}
+      okButtonProps={{ style: { display: 'none' } }}>
       <div
         style={{
-          padding: "20px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          fontSize: "16px",
-          color: "#474747"
-        }}
-      >
+          padding: '20px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          fontSize: '16px',
+          color: '#474747',
+        }}>
         <Image
-          alt="/"
+          alt='/'
           width={50}
           height={50}
-          src={"/big-x.png"}
-          style={{ marginBottom: "20px" }}
+          src={'/big-x.png'}
+          style={{ marginBottom: '20px' }}
         />
-        <p style={{ textAlign: "center" }}>
+        <p style={{ textAlign: 'center' }}>
           Việc xóa tùy chọn có thể ảnh hưởng đến kết quả tính lương của các
           tháng trước. Bạn có chắc chắn muốn xóa tùy chọn này!
         </p>
-        <div style={{ marginTop: "20px" }}>
+        <div style={{ marginTop: '20px' }}>
           <Button
             style={{
-              padding: "5px 40px 5px 40px",
-              height: "auto",
-              backgroundColor: "white"
+              padding: '5px 40px 5px 40px',
+              height: 'auto',
+              backgroundColor: 'white',
             }}
-            onClick={() => setOpen(false)}
-          >
-            <p style={{ color: "#4C5BD4", fontSize: "18px" }}>Hủy</p>
+            onClick={() => setOpen(false)}>
+            <p style={{ color: '#4C5BD4', fontSize: '18px' }}>Hủy</p>
           </Button>
           <Button
             style={{
-              marginLeft: "20px",
-              padding: "5px 30px 5px 30px",
-              height: "auto",
-              backgroundColor: "#4C5BD4"
+              marginLeft: '20px',
+              padding: '5px 30px 5px 30px',
+              height: 'auto',
+              backgroundColor: '#4C5BD4',
             }}
-            onClick={onDel}
-          >
-            <p style={{ color: "white", fontSize: "18px" }}>Đồng ý</p>
+            onClick={onDel}>
+            <p style={{ color: 'white', fontSize: '18px' }}>Đồng ý</p>
           </Button>
         </div>
       </div>
@@ -95,18 +91,18 @@ export const ModalUpDateCaiDatDiMuonVeSom = (
   const router = useRouter()
   const initialValues = {
     ...selectedRow,
-    pm_time_begin: moment(selectedRow?.pm_time_begin)?.format("YYYY-MM"),
+    pm_time_begin: moment(selectedRow?.pm_time_begin)?.format('YYYY-MM'),
 
-    pm_time_end: moment(selectedRow?.pm_time_end)?.format("YYYY-MM")
+    pm_time_end: moment(selectedRow?.pm_time_end)?.format('YYYY-MM'),
   }
 
   const onFinish = async (value) => {
     if (value) {
-      const res = await POST_TL("api/tinhluong/congty/update_phat_muon", {
+      const res = await POST_TL('api/tinhluong/congty/update_phat_muon', {
         ...value,
         pm_monney: parseInt(value?.pm_monney),
         pm_id_com: 3312,
-        pm_id: selectedRow?.pm_id
+        pm_id: selectedRow?.pm_id,
       })
 
       if (res?.data) {
@@ -123,15 +119,14 @@ export const ModalUpDateCaiDatDiMuonVeSom = (
       onCancel={() => setOpen(false)}
       width={600}
       closable={false}
-      cancelButtonProps={{ style: { display: "none" } }}
-      okButtonProps={{ style: { display: "none" } }}
-    >
+      cancelButtonProps={{ style: { display: 'none' } }}
+      okButtonProps={{ style: { display: 'none' } }}>
       <div className={styles.header}>
         <div></div>
         <div className={styles.textHead}>Chỉnh sửa mức phạt đi muộn về sớm</div>
         <Image
-          alt="/"
-          src={"/cross.png"}
+          alt='/'
+          src={'/cross.png'}
           width={14}
           height={14}
           onClick={() => setOpen(false)}
@@ -140,82 +135,70 @@ export const ModalUpDateCaiDatDiMuonVeSom = (
       <div className={styles.body}>
         <Form form={form} onFinish={onFinish} initialValues={initialValues}>
           <Form.Item
-            name="pm_type"
-            label="Lý do phạt"
+            name='pm_type'
+            label='Lý do phạt'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
             <Select
-              placeholder="Chọn lí do"
-              size="large"
-              suffixIcon={<img src="/down-icon.png"></img>}
-            ></Select>
+              placeholder='Chọn lí do'
+              size='large'
+              suffixIcon={<img src='/down-icon.png'></img>}></Select>
           </Form.Item>
           <Form.Item
-            name="pm_shift"
-            label="Ca làm việc áp dụng"
+            name='pm_shift'
+            label='Ca làm việc áp dụng'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
             <Select
-              placeholder="Tất cả các ca"
-              size="large"
-              suffixIcon={<img src="/down-icon.png"></img>}
-            ></Select>
+              placeholder='Tất cả các ca'
+              size='large'
+              suffixIcon={<img src='/down-icon.png'></img>}></Select>
           </Form.Item>
           <Form.Item
-            name="pm_minute"
-            label="Số phút áp dụng mức phạt"
+            name='pm_minute'
+            label='Số phút áp dụng mức phạt'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
             <Input
-              placeholder="Nhập số phút"
-              type="number"
-              size="large"
-            ></Input>
+              placeholder='Nhập số phút'
+              type='number'
+              size='large'></Input>
           </Form.Item>
           <Form.Item
-            name="pm_type_phat"
-            label="Loại phạt"
+            name='pm_type_phat'
+            label='Loại phạt'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
             <Select
-              placeholder="Chọn phương thức"
-              size="large"
-              suffixIcon={<img src="/down-icon.png"></img>}
-            ></Select>
+              placeholder='Chọn phương thức'
+              size='large'
+              suffixIcon={<img src='/down-icon.png'></img>}></Select>
           </Form.Item>
           <Form.Item
-            name="pm_monney"
-            label="Số tiền hoặc công phạt"
+            name='pm_monney'
+            label='Số tiền hoặc công phạt'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
             <Input
-              placeholder="Nhập số phút"
-              type="number"
-              size="large"
-            ></Input>
+              placeholder='Nhập số phút'
+              type='number'
+              size='large'></Input>
           </Form.Item>
           <Form.Item
-            name="pm_time_begin"
-            label="Thời gian bắt đầu áp dụng    "
+            name='pm_time_begin'
+            label='Thời gian bắt đầu áp dụng    '
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
-            <Input type="month" size="large"></Input>
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
+            <Input type='month' size='large'></Input>
           </Form.Item>
           <Form.Item
-            name="pm_time_end"
-            label="Thời gian kết thúc"
-            labelCol={{ span: 24 }}
-          >
-            <Input type="month" size="large"></Input>
+            name='pm_time_end'
+            label='Thời gian kết thúc'
+            labelCol={{ span: 24 }}>
+            <Input type='month' size='large'></Input>
           </Form.Item>
           <Form.Item className={styles.formButton}>
-            <Button htmlType="submit" className={styles.button}>
+            <Button htmlType='submit' className={styles.button}>
               Cập nhật
             </Button>
           </Form.Item>
@@ -229,15 +212,16 @@ export const ModalCaiDatDiMuonVeSom = (
   open: boolean,
   setOpen: Function,
   data: any,
-  setData: any
+  setData: any,
+  listCa: any[]
 ) => {
   const [form] = Form.useForm()
   const router = useRouter()
   const onFinish = async (value) => {
     if (value) {
-      const res = await POST_TL("api/tinhluong/congty/insert_phat_muon", {
+      const res = await POST_TL('api/tinhluong/congty/insert_phat_muon', {
         ...value,
-        pm_id_com: 3312
+        pm_id_com: 3312,
       })
 
       if (res?.data) {
@@ -256,15 +240,14 @@ export const ModalCaiDatDiMuonVeSom = (
       width={600}
       destroyOnClose
       closable={false}
-      cancelButtonProps={{ style: { display: "none" } }}
-      okButtonProps={{ style: { display: "none" } }}
-    >
+      cancelButtonProps={{ style: { display: 'none' } }}
+      okButtonProps={{ style: { display: 'none' } }}>
       <div className={styles.header}>
         <div></div>
         <div className={styles.textHead}>Thêm mới mức phạt đi muộn về sớm</div>
         <Image
-          alt="/"
-          src={"/cross.png"}
+          alt='/'
+          src={'/cross.png'}
           width={14}
           height={14}
           onClick={() => setOpen(false)}
@@ -273,88 +256,88 @@ export const ModalCaiDatDiMuonVeSom = (
       <div className={styles.body}>
         <Form form={form} onFinish={onFinish}>
           <Form.Item
-            name="pm_type"
-            label="Lý do phạt"
+            name='pm_type'
+            label='Lý do phạt'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt buộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt buộc' }]}>
             <Select
-              placeholder="Chọn lí do"
-              size="large"
-              suffixIcon={<img src="/down-icon.png"></img>}
+              placeholder='Chọn lí do'
+              size='large'
+              suffixIcon={<img src='/down-icon.png'></img>}
               options={[
-                { value: 1, label: 1 },
-                { value: 2, label: 2 }
-              ]}
-            ></Select>
+                { value: 1, label: 'Phạt đi muộn' },
+                { value: 0, label: 'Phạt về sớm' },
+              ]}></Select>
           </Form.Item>
           <Form.Item
-            name="pm_shift"
-            label="Ca làm việc áp dụng"
+            name='pm_shift'
+            label='Ca làm việc áp dụng'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
             <Select
-              placeholder="Tất cả các ca"
-              size="large"
-              suffixIcon={<img src="/down-icon.png"></img>}
-              options={[{ value: 4412, label: 4412 }]}
-            ></Select>
+              placeholder='Tất cả các ca'
+              size='large'
+              suffixIcon={<img src='/down-icon.png'></img>}
+              options={
+                listCa &&
+                listCa?.map((item) => ({
+                  value: item?.shift_id,
+                  label: item?.shift_name,
+                }))
+              }></Select>
           </Form.Item>
           <Form.Item
-            name="pm_minute"
-            label="Số phút áp dụng mức phạt"
+            name='pm_minute'
+            label='Số phút áp dụng mức phạt'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
             <Input
-              placeholder="Nhập số phút"
-              type="number"
-              size="large"
-            ></Input>
+              placeholder='Nhập số phút'
+              type='number'
+              size='large'></Input>
           </Form.Item>
           <Form.Item
-            name="pm_type_phat"
-            label="Loại phạt"
+            name='pm_type_phat'
+            label='Loại phạt'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
             <Select
-              placeholder="Chọn phương thức"
-              size="large"
-              suffixIcon={<img src="/down-icon.png"></img>}
-              options={[{ value: 1, label: 1 }]}
-            ></Select>
+              placeholder='Chọn phương thức'
+              size='large'
+              suffixIcon={<img src='/down-icon.png'></img>}
+              options={[
+                { value: 1, label: 'Phạt tiền / ca' },
+                {
+                  value: 0,
+                  label: 'Phạt công/ ca ',
+                },
+              ]}></Select>
           </Form.Item>
           <Form.Item
-            name="pm_monney"
-            label="Số tiền hoặc công phạt"
+            name='pm_monney'
+            label='Số tiền hoặc công phạt'
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
             <Input
-              placeholder="Nhập số phút"
-              type="number"
-              size="large"
-            ></Input>
+              placeholder='Nhập số phút'
+              type='number'
+              size='large'></Input>
           </Form.Item>
           <Form.Item
-            name="pm_time_begin"
-            label="Thời gian bắt đầu áp dụng    "
+            name='pm_time_begin'
+            label='Thời gian bắt đầu áp dụng    '
             labelCol={{ span: 24 }}
-            rules={[{ required: true, message: "Trường này là bắt luộc" }]}
-          >
-            <Input type="month" size="large"></Input>
+            rules={[{ required: true, message: 'Trường này là bắt luộc' }]}>
+            <Input type='month' size='large'></Input>
           </Form.Item>
           <Form.Item
-            name="pm_time_end"
-            label="Thời gian kết thúc"
-            labelCol={{ span: 24 }}
-          >
-            <Input type="month" size="large"></Input>
+            name='pm_time_end'
+            label='Thời gian kết thúc'
+            labelCol={{ span: 24 }}>
+            <Input type='month' size='large'></Input>
           </Form.Item>
           <Form.Item className={styles.formButton}>
-            <Button htmlType="submit" className={styles.button}>
+            <Button htmlType='submit' className={styles.button}>
               Lưu
             </Button>
           </Form.Item>
