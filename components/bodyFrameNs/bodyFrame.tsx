7c8172de63653ea1c6d0f5cc314dbb5a7e2fcd9a
LongTien15 <nguyentienlong.150800@gmail.com>
2023-08-19 12:08:10 +0700

    fix ui

--- components/bodyFrameNs/bodyFrame.tsx
+++ components/bodyFrameNs/bodyFrame.tsx
@@ -131,12 +131,19 @@ export default function Bodyframe({ children }: any) {
         <Row gutter={[20, 20]} className={`bannerQLC ${styles.moreSection}`}>
           <Col lg={6} md={8} sm={8} xs={24}>
             <Card className={styles.cardArticleSection}>
-              <IndexSection className={styles.idxSection} />
+              <IndexSection
+                className={styles.idxSection}
+                setShowmore={setShowmore}
+              />
             </Card>
           </Col>
           <Col lg={18} md={16} sm={16} xs={24}>
             <Card className={styles.cardArticleSection}>
-              <Article className={styles.articleSection} showMore={showMore} />
+              <Article
+                className={styles.articleSection}
+                showMore={showMore}
+                setShowmore={setShowmore}
+              />
             </Card>
             <div className={styles.btnWrapper}>
               <Button
