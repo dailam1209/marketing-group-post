c535a7e6325b61a9852b9b4778017f14b83a8b52
quangzu <82556917+truongquangvu09@users.noreply.github.com>
2023-08-21 17:33:38 +0700

    fixed:router logout hr

--- components/hr/logout/index.tsx
+++ components/hr/logout/index.tsx
@@ -13,7 +13,7 @@ export default function LogoutHr({ setShowLogout, showLogout }: any) {
         Cookies.remove("rf_token");
         Cookies.remove("role");
         Cookies.remove("phone");
-        window.location.href = "/trang-chu-truoc-dang-nhap";
+        window.location.href = "/phan-mem-nhan-su";
     };
 
     useEffect(() => {
