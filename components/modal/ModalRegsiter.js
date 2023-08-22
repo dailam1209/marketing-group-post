7960187778ea4ae052840f2342ab810a5b8b3957
Van <phonglang01249@gmail.com>
2023-08-22 17:33:24 +0700

    sua loi dang ky/danh nhap dieu huong tu raonhanh365

--- components/modal/ModalRegsiter.js
+++ components/modal/ModalRegsiter.js
@@ -1,10 +1,20 @@
 /** @format */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Modal.module.scss';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-export default function ModalRegsiter({setOpenModalRegister}) {
+export default function ModalRegsiter({ setOpenModalRegister }) {
+   const router = new useRouter();
+   const [getUrl, setUrl] = useState('');
+   useEffect(() => {
+      const query = router.query;
+      if (query.url && query.urlRedeict) {
+         const urlGet = '?url=' + query.url + '&urlRedeict=' + query.urlRedeict;
+         setUrl(urlGet);
+      }
+   }, [])
    return (
       <div>
          <div className={styles.modal_login_register}>
@@ -19,19 +29,19 @@ export default function ModalRegsiter({setOpenModalRegister}) {
             <div className={styles.content}>
                <div className={styles.text}>{`Để tiếp tục đăng ký bạn vui lòng chọn loại tài khoản.`}</div>
                <div className={styles.khoi}>
-                  <Link href='dang-ky-cong-ty.html'>
+                  <Link href={`dang-ky-cong-ty.html${getUrl}`}>
                      <div className={styles.khoi_item}>
                         <img src='./Home_fill.png' alt='' />
                         <span>Công ty</span>
                      </div>
                   </Link>
-                  <Link href='dang-ky-nhan-vien.html'>
+                  <Link href={`dang-ky-nhan-vien.html${getUrl}`}>
                      <div className={styles.khoi_item}>
                         <img src='./User_alt_fill.png' alt='' />
                         <span>Nhân viên</span>
                      </div>{' '}
                   </Link>
-                  <Link href='dang-ky-ca-nhan.html'>
+                  <Link href={`dang-ky-ca-nhan.html${getUrl}`}>
                      <div className={styles.khoi_item}>
                         <img src='./User_circle.png' alt='' />
                         <span>Cá nhân</span>
