1b4c3f0db3bf50a2ef0130fd78ee013107f50722
Huu Dat <huudat@Apples-MacBook-Pro-of-t.local>
2023-08-29 09:14:05 +0700

    update

--- pages/crm/marketing/sms/add.tsx
+++ pages/crm/marketing/sms/add.tsx
@@ -40,7 +40,7 @@ const AddFilesCustomerList: React.FC = () => {
         <meta name="viewport" content="width=device-width" initial-scale="1" />
         <meta name="robots" content="noindex,nofollow" />
         <title>
-          CRM 365 - đáp án của bài toán tối ưu quy trình, gia tăng lợi nhuận
+          Thêm mới SMS
         </title>
         <meta
           name="description"
