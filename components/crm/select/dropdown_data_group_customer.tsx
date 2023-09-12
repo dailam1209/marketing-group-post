0a963b34db8ede2992e95a2fc789066e78bed61b
dung <tranthideptrai@gmail.com>
2023-09-13 00:01:53 +0700

    dungtqd-fix

--- components/crm/select/dropdown_data_group_customer.tsx
+++ components/crm/select/dropdown_data_group_customer.tsx
@@ -73,7 +73,6 @@ export default function CustomerGroupSelectDropdownData({
     str = str.replace(/\s+/g, " ").trim();
     return str;
   }
-  console.log("arr", arr);
   useEffect(() => {
     const newData = data?.filter((item) => {
       return removeVietnameseDiacritics(item.gr_name)?.includes(
@@ -85,14 +84,13 @@ export default function CustomerGroupSelectDropdownData({
         removeVietnameseDiacritics(valueFilter)
       );
     });
-    console.log(newData,newData2);
     if (newData[0]?.lists_child) {
       setFilterData(newData);
     } else {
       setFilterData(newData2);  
     }
   }, [valueFilter]);
-  console.log("first,", filterData);
+
   return (
     <span
       className={`${styles.select2_container_open} ${styles.select2_container} ${styles.select2_container_default} `}
